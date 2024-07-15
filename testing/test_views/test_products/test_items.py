bd43485e34033eebc83d17ab7e315e40ce73246f
Quill Craftsman <craftsman@localhost.localdomain>
2024-07-15 21:27:45 +0300

    add new tests

--- testing/test_views/test_products/test_items.py
+++ testing/test_views/test_products/test_items.py
@@ -4,3 +4,8 @@ def test_get_items(client):
     assert response.json == {
         "items": ["a", "b"],
     }
+
+
+def test_get_item_by_id(client):
+    response = client.get('/items/45/')
+    assert response.status_code == 200
